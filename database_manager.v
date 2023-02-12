fc6037523c9abac94864bbf341c812a5f55015ab
RIC217 <melericcardo9400@gmail.com>
2023-02-12 17:57:49 +0100

    Dispatched database_manager.v functionalities in several functions

--- database_manager.v
+++ database_manager.v
@@ -25,74 +25,15 @@ fn main() {
 	for {
 		mut command := os.input("> ")
 		command_lower := command.to_lower()
-		if command_lower == "help" || command == "?" {
-			println("Commands :")
-			println("help/? => Display this help.")
-			println("list => Display all accounts.")
-			println("     -a => Display all infos about each account.")
-			println("add <username> <password> => Add an account to the database.")
-			println("exit => Exit the program.")
-		}
-		if command_lower == "exit" {
-			break
-		}
-		if command_lower.starts_with("list") {
-			mut verbose := false
-			if command.ends_with("-a") {
-				verbose = true
-			}
-			for account in sql db {
-				select from Account
-			} {
-				println("------------------------------")
-				println("Account n°${account.id}")
-				println("Username : ${account.username}")
-				if verbose {
-					println("Password : ${account.password}")
-					println("Salt : ${account.salt}")
-				}
-			}
-		}
-		if command_lower.starts_with("add ") {
-			command = command[4..]
-			if command.len < 7 {
-				println("Syntax : add <username> <password>")
-				continue
-			}
-			infos := command.split(" ")
-			if infos.len < 2 {
-				println("Syntax : add <username> <password>")
-				continue
-			}
-			username := infos[0]
-			if username.len < 3 {
-				println("Length of username must be greater than 2 !")
-				continue
-			}
-			password := infos[1]
-			mut error := false
-			for i, c in username {
-				if i == 0 {
-					if !c.is_letter() {
-						error = true
-						break
-					}
-				} else {
-					if !c.is_alnum() && c != "_".u8() {
-						error = true
-						break
-					}
-				}
-			}
-			if error {
-				println("Username must begin with a letter and contains only letters, numbers and underscores !")
-				continue
-			}
-			if password.len < 8 {
-				println("Password must be at least 8 characters long !")
-				continue
-			}
-			insert_account(username, password, db)
+		if command_lower=="help" || command_lower=="?" {
+			help_command()
+		} else if command_lower=="exit" { return }
+		else if command_lower.starts_with("list") {
+			list_command(command, db)
+		} else if command_lower.starts_with("add ") {
+			add_command(command, db)
+		} else if !command_lower.is_blank() {
+			println("Command not found !")
 		}
 	}
 }
@@ -110,4 +51,73 @@ fn insert_account(username string, clear_password string, db sqlite.DB) {
 	}
 
 	println("Successfully added account in the database !")
+}
+
+fn help_command() {
+	println("Commands :")
+	println("help/? => Display this help.")
+	println("list => Display all accounts.")
+	println("     -a => Display all infos about each account.")
+	println("add <username> <password> => Add an account to the database.")
+	println("exit => Exit the program.")
+}
+
+fn list_command(command string, db sqlite.DB) {
+	mut verbose := false
+	if command.ends_with("-a") {
+		verbose = true
+	}
+	for account in sql db {
+		select from Account
+	} {
+		println("------------------------------")
+		println("Account n°${account.id}")
+		println("Username : ${account.username}")
+		if verbose {
+			println("Password : ${account.password}")
+			println("Salt : ${account.salt}")
+		}
+	}
+}
+
+fn add_command(command_temp string, db sqlite.DB) {
+	command := command_temp[4..]
+	if command.len < 7 {
+		println("Syntax : add <username> <password>")
+		return
+	}
+	infos := command.split(" ")
+	if infos.len < 2 {
+		println("Syntax : add <username> <password>")
+		return
+	}
+	username := infos[0]
+	if username.len < 3 {
+		println("Length of username must be greater than 2 !")
+		return
+	}
+	password := infos[1]
+	mut error := false
+	for i, c in username {
+		if i == 0 {
+			if !c.is_letter() {
+				error = true
+				break
+			}
+		} else {
+			if !c.is_alnum() && c != "_".u8() {
+				error = true
+				break
+			}
+		}
+	}
+	if error {
+		println("Username must begin with a letter and contains only letters, numbers and underscores !")
+		return
+	}
+	if password.len < 8 {
+		println("Password must be at least 8 characters long !")
+		return
+	}
+	insert_account(username, password, db)
 }

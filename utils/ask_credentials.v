90399858f0106ed9f121b9e40edc29a033297975
RIC217 <melericcardo9400@gmail.com>
2023-02-15 21:13:18 +0100

    Optimised code

--- utils/ask_credentials.v
+++ utils/ask_credentials.v
@@ -7,54 +7,54 @@ pub fn (mut app App) ask_credentials(mut user &User) (string, string) {
 		return "Cannot read credentials", ""
 	}
 	//removing null bytes
-	data = data[..data_length]
+	data = data#[..data_length]
+	if data.len == 0 {
+		return "Bad credentials 0", ""
+	}
 	mut credentials_string := data.bytestr()
 	//getting mode
-	if credentials_string.len < 10 {
-		if user.send_message("1Bad credentials") {
-			return "Error while sending bad credentials 1 !", ""
-		}
-		return "Bad credentials 1", ""
+	credentials_string_length := credentials_string#[..5].int()
+	if credentials_string_length == 0 {
+		return "Bad credentials length !", ""
 	}
-	credentials_string_length := credentials_string[..5].int()
-	credentials_string = credentials_string[5..]
-	if credentials_string.len < credentials_string_length {
-		if user.send_message("1Bad credentials") {
-			return "Error while sending bad credentials 2 !", ""
-		}
-		return "Bad credentials 2", ""
+	credentials_string = credentials_string#[5..]
+	if credentials_string.len == 0 {
+		return "Credentials too short !", ""
 	}
-	credentials_string = credentials_string[..credentials_string_length]
-	if credentials_string.len < 2 {
-		if user.send_message("1Bad credentials") {
-			return "Error while sending bad credentials 3 !", ""
-		}
-		return "Bad credentials 3", ""
+	credentials_string = credentials_string#[..credentials_string_length]
+	if credentials_string.len <= 1 {
+		return "Bad credentials 3 !", ""
 	}
 	mode := credentials_string[0].ascii_str()
-	credentials_string = credentials_string[1..]
+	credentials_string = credentials_string#[1..]
+	if credentials_string.len == 0 {
+		return "Credentials too short !", ""
+	}
 	//getting username length
-	username_length := credentials_string[..2].int()
-	credentials_string = credentials_string[2..]
-	if credentials_string.len < username_length {
-		if user.send_message("1Bad credentials") {
-			return "Error while sending bad credentials 4 !", ""
-		}
-		return "Bad credentials 4", ""
+	username_length := credentials_string#[..2].int()
+	if username_length == 0 {
+		return "Bad username length !", ""
+	}
+	credentials_string = credentials_string#[2..]
+	if credentials_string.len == 0 {
+		return "Bad credentials !", ""
 	}
 	// getting username
-	username := credentials_string[..username_length]
-	credentials_string = credentials_string[username_length..]
-	//getting password
-	if credentials_string.len < 64 {
-		if user.send_message("1Bad credentials") {
-			return "Error while sending bad credentials 6 !", ""
-		}
-		return "Bad credentials 6", ""
+	username := credentials_string#[..username_length]
+	if username.len == 0 {
+		return "Username too short !", ""
 	}
-	password := credentials_string[..64]
+	credentials_string = credentials_string#[username_length..]
+	if credentials_string.len == 0 {
+		return "Credentials too short !", ""
+	}
+	//getting password
+	password := credentials_string#[..64]
+	if password.len == 0 {
+		return "Password too short !", ""
+	}
 	if credentials_string.len > 64 {
-		println("[LOG] More characters than expected, message : `${credentials_string[64..]}`")
+		eprintln("[LOG] More characters than expected, message : `${credentials_string[64..]}`")
 	}
 	match mode {
 		"l" { return app.login(mut user, username, password) }
